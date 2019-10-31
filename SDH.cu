f3d7e239b7d92c76034e11882894895f3c3b30b5
Alexander Cook <awcook@mail.usf.edu>
2019-10-31 17:43:05 -0400

    Current fastest version: T = 31.876 for 10000 atoms 32 threads/block

--- SDH.cu
+++ SDH.cu
@@ -40,85 +40,47 @@ int num_buckets;		/* total number of buckets in the histogram */
 double   PDH_res;		/* value of w                               */
 atom * atom_list;		/* list of all data points					*/
 
-__device__ double d_p2p_distance(double x1, double y1, double z1, atom atom2) {
+//Device helper function: Distance of two points in the atom_list
+//Device helper function which now takes a pointer as an argument instead of using a global pointer
+__device__ double d_p2p_distance(atom *atom_list, int ind1, int ind2) {
 	
-	double x2 = atom2.x_pos;
-	double y2 = atom2.y_pos;
-	double z2 = atom2.z_pos;
+	double x1 = atom_list[ind1].x_pos;
+	double x2 = atom_list[ind2].x_pos;
+	double y1 = atom_list[ind1].y_pos;
+	double y2 = atom_list[ind2].y_pos;
+	double z1 = atom_list[ind1].z_pos;
+	double z2 = atom_list[ind2].z_pos;
 		
 	return sqrt((x1 - x2)*(x1-x2) + (y1 - y2)*(y1 - y2) + (z1 - z2)*(z1 - z2));
 }
 
 
 __global__ void PDH_kernel(atom *d_atom_list, bucket *d_histogram, int PDH_acnt, int PDH_res, int num_buckets){
-	
-	//variables
-	int B = blockDim.x; //block size
-	int M = gridDim.x; //total number of blocks
-	int T = blockIdx.x * blockDim.x + threadIdx.x; //thread id in grid (global)
-	int t = threadIdx.x; //thread id within current block (local)
-	int b = blockIdx.x; //block id
+	int T = blockIdx.x * blockDim.x + threadIdx.x;
+	int t = threadIdx.x;
 
-	extern __shared__ int s[]; //one shared memory array for input and output
+	extern __shared__ int s_histogram[];
 
-	int *s_histogram = s; //pointer to first portion of shared memory (output)
-	atom *R_atom_list = (atom*)&s_histogram[num_buckets]; //pointer to second portion (input)
-
-	//initialize output array to 0 in block sized chunks
-	for(int i = t; i < num_buckets; i += B){
+	for(int i = t; i < num_buckets; i += blockDim.x){ //initialize array to 0 in block sized chunks
 		s_histogram[i] = 0;
 	}
 
-	//load L into registers for inter-block computation
-	int Lx = d_atom_list[T].x_pos;
-	int Ly = d_atom_list[T].y_pos;
-	int Lz = d_atom_list[T].z_pos;
-	
 	__syncthreads();
  
 	int h_pos;
 	double dist;
-	
-	//nested loop for inter-block computation
-	for(int i = b + 1; i < M; i++){ //b+1, because intra-block distance is a later step
 
-		//initialize block R
-		T = i * B + t; //need to load "i-th block" away from the current block
-		if(T < PDH_acnt){
-			R_atom_list[t] = d_atom_list[T]; //load from global thread index to local thread index
-		}
-
-		__syncthreads();
-
-		for(int j = 0; j < B && (i * B + j) < PDH_acnt; j++){ //each block, 'L' computes distances between its own atoms and every other block 'R'
-			dist = d_p2p_distance(Lx, Ly, Lz, R_atom_list[j]);
-			h_pos = (int) (dist / PDH_res);
-			atomicAdd(&(s_histogram[h_pos]), 1);
-		}
-		__syncthreads(); //second sync necessary because the outer loop will start loading before the inner loop is done
-	}
-
-	atom *L_atom_list = R_atom_list; //reuse R to cut down on shared memory use
-
-	//load Block L for intra-block computation
-	if(T < PDH_acnt){
-		L_atom_list[t] = d_atom_list[T];
-		}
-
-	__syncthreads();
-	
-	//loop for intra-block computation
-	for(int i = t + 1; i < B && (b * B + i) < PDH_acnt; i++){ //each block computes distances between its own atoms
-		dist = d_p2p_distance(Lx, Ly, Lz, L_atom_list[i]);
+	for(int i = T + 1; i < PDH_acnt; i++){
+		dist = d_p2p_distance(d_atom_list, T, i);
 			h_pos = (int) (dist / PDH_res);
 			atomicAdd(&(s_histogram[h_pos]), 1);
 			 
 	}
 
-	__syncthreads(); //finish writing to shared memory
+	__syncthreads();
 
 	//reduce shared output into global output
-	for(int i = t; i < num_buckets; i += B){ //output to global memory in block sized chunks
+	for(int i = t; i < num_buckets; i += blockDim.x){ //output to global memory in block sized chunks
 		atomicAdd(&(d_histogram[i].d_cnt), s_histogram[i]);
 	}
 
@@ -150,8 +112,8 @@ int main(int argc, char **argv)
 
 	PDH_acnt = atoi(argv[1]);
 	PDH_res	 = atof(argv[2]);
-	int num_threads = atoi(argv[3]);
-
+    int num_threads = atoi(argv[3]);
+    
 	//Allocate host memory
 	num_buckets = (int)(BOX_SIZE * 1.732 / PDH_res) + 1;
 	h_histogram = (bucket *)malloc(sizeof(bucket) * num_buckets);
@@ -179,7 +141,8 @@ int main(int argc, char **argv)
 	cudaMemcpy(d_histogram, h_histogram, sizeof(bucket)*num_buckets, cudaMemcpyHostToDevice);
 	cudaMemcpy(d_atom_list, atom_list, sizeof(atom)*PDH_acnt, cudaMemcpyHostToDevice);
 
-	//Define grid size
+	//Define block and grid size
+	//int num_threads = 256; //number of threads in one dimension of a block
 	int num_blocks = (PDH_acnt + num_threads - 1)/num_threads; //calculate number of blocks needed
 	
 	//Start counting time
@@ -189,7 +152,7 @@ int main(int argc, char **argv)
 	cudaEventRecord(start, 0);
 
 	//Launch kernel
-	PDH_kernel<<<num_blocks,num_threads, sizeof(bucket)*num_buckets + sizeof(atom)*num_threads>>>(d_atom_list, d_histogram, PDH_acnt, PDH_res, num_buckets);
+	PDH_kernel<<<num_blocks,num_threads, sizeof(int)*num_buckets>>>(d_atom_list, d_histogram, PDH_acnt, PDH_res, num_buckets);
 	//PDH_kernelST<<<1,1>>>(d_atom_list, d_histogram, PDH_acnt, PDH_res);
 
 	//stop counting time
